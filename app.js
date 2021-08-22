d02e21ee279dd0b994f0f382e9c0ce277f2c480f
Trux <kaithomasdean@outlook.com>
2021-08-21 15:05:36 -1000

    initial

---
+++ app.js
@@ -0,0 +1,22 @@
+async function getGif(term) {
+  const payload = { api_key: "PPTJfgGAkO3r5ArIBLqtM9PQxUdC0pTL", tag: term };
+  const img = await axios.get("http://api.giphy.com/v1/gifs/random", {
+    params: payload,
+  });
+  createGif(img.data.data.images.downsized_medium.url)
+}
+$("form").on("submit", (e) => {
+  e.preventDefault();
+  const tag = $("#tag").val();
+  getGif(tag)
+});
+function createGif(url) {
+    const container = $('#container')
+    const img = document.createElement('img')
+    img.src = url
+    container.append(img)
+}
+
+$('#erase').on('click', (e) => {
+    $('#container').empty()
+})
